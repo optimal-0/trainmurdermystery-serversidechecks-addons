8654ca8c17179910f50f3cdef2318f51bb57330a
optimal-0 <optimal0efficiency@gmail.com>
2025-11-28 21:25:32 -0800

    start

---
+++ src/main/java/com/trainmurdermysteryserversidechecks/TrainMurderMysteryServerSideChecks.java
@@ -0,0 +1,17 @@
+package com.trainmurdermysteryserversidechecks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TrainMurderMysteryServerSideChecks implements ModInitializer {
+	public static final String MOD_ID = "trainmurdermysteryserversidechecks";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/trainmurdermysteryserversidechecks/client/TMMEClient.java
@@ -0,0 +1,11 @@
+package com.trainmurdermysteryserversidechecks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TMMEClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
